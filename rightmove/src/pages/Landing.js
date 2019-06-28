15e811ecad747cdb564d073207dda92462a0a9da
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-06-27 22:08:16 -0400

    get landing page styling right

--- rightmove/src/pages/Landing.js
+++ rightmove/src/pages/Landing.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Container from '../components/Container'
+import Container from '../components/Container';
+import './Landing.css';
 
 function Landing() {
     return (
         <Container>
-            <h1>hello this is the landing page</h1>
+            <div className="Catchy-Tagline">Catchy Tagline</div>
+            <p className="Body-Copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod f  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <button className="Button -Button-Text">Create account with LinkedIn</button>
         </Container>
     )
 
