800198ff313012b8e6d6518968cd7bb1552b0df3
Kasie <kchaplick92@gmail.com>
2019-06-29 12:54:29 -0400

    added Row and container to landing page

--- rightmove/src/components/Container/index.js
+++ rightmove/src/components/Container/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './style.css';
 
 const Container = (props) => {
-  return <div className="Content-Container">{props.children}</div>;
+  return <div className="container Content-Container">{props.children}</div>;
 }
 
 export default Container;
--- rightmove/src/pages/Landing/index.js
+++ rightmove/src/pages/Landing/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Container from '../../components/Container';
+import Row from '../../components/Row';
 import './style.css';
 
 function Landing() {
     return (
         <Container>
+          <Row>
             <div className="Catchy-Tagline">Catchy Tagline</div>
+          </Row>
             <p className="Body-Copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod f  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <button className="Button -Button-Text">Create account with LinkedIn</button>
         </Container>
