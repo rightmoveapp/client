ab7affe19a8099fdff749f185947c65f05de879f
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-03 22:21:47 -0400

    change styling on the footer

--- rightmove/src/components/Footer/index.js
+++ rightmove/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css'
 
 const Footer = () => {
-    return <footer className="footer Footer-Area Placeholder page-footer">Footer w Credits</footer>;
+    return <footer className="footer Footer-Area Placeholder page-footer">Made with ❤ by Team RedStapler</footer>;
 }
 
 export default Footer;
