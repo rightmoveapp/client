91458d3b13408bea5044248beeda638f0579a656
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-04 23:07:56 -0400

    make sure that the yellow button can take in links

--- rightmove/src/components/YellowButton/index.js
+++ rightmove/src/components/YellowButton/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './style.css'
 
 const YellowButton = props => {
-    return <button className="YellowButton -YellowButton-Text">{props.text}</button>;
+    return (
+        <Link to={props.to}>
+            <button className="YellowButton -YellowButton-Text">{props.text}</button>
+        </Link>
+    )
 }
 
 export default YellowButton;
