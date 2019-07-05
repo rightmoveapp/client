126a207a440c331ee9a4388c760afbdd2dcbefc6
Kasie <kchaplick92@gmail.com>
2019-07-04 20:07:50 -0400

    updated app.js with new components

--- rightmove/src/App.js
+++ rightmove/src/App.js
@@ -4,24 +4,29 @@ import Nav from "./components/Nav";
 import Landing from './pages/Landing';
 import LinkedinAuth from './pages/LinkedinAuth';
 import Account from './pages/Account';
+import PrivateRoute from './PrivateRoute';
+import LogOut from './pages/LogOut'
 /*import Questions from './pages/Questions';
 import Job from './pages/Job';
 import CurrentState from './pages/CurrentState'; */
 import './App.css';
+import Cookies from 'universal-cookie';
 
+const cookies = new Cookies();
+const loggedIn = (cookies.get("token") !== "undefined")
 
 function App() {
   return (
     <Router>
-      <Nav name="Steve"/>
+      <Nav name="Steve" isLogginedIn={loggedIn}/>
         <Switch>
-          <Route exact path="/v1/login" render={props => <Account {...props}/>} />
+          <Route exact path="/logout" component={LogOut} />
+          <PrivateRoute path="/account" component={Account} />
           <Route exact path="/linkedin_auth" component={LinkedinAuth} />
           <Route exact path="/" component={Landing}  />
-          {/* <Route exact path="/account" component={Account} />
-          <Route exact path="/questions" component={Questions} />
+          {/* <Route exact path="/questions" component={Questions} />
           <Route exact path="/job_detail" component={Job} />
-          <Route exact path="/current_state" component={CurrentState} /> */}
+          <Route exact path="/current_state" component={CurrentState} /> */} 
         </Switch>
     </Router>
   );
--- rightmove/src/pages/LinkedinAuth/index.js
+++ rightmove/src/pages/LinkedinAuth/index.js
@@ -2,32 +2,39 @@ import React, { Component } from "react";
 import { Redirect } from 'react-router-dom'
 import queryString from 'query-string'
 import Cookies from 'universal-cookie';
+import axios from "axios";
 
 const cookies = new Cookies();
+const apihost = "http://localhost:8000/api/v1/login"
 
 class LinkedinAuth extends Component {
-
 
     componentDidMount() {
         const values = queryString.parse(this.props.location.search)
         console.log(`value code ${values.code}`)
-        const token = values.code
-        cookies.set("token", token, {path:"/"})
-        console.log(`my cookie is ${cookies.get('token')}`);
-        this.renderRedirect();
+        const authCode = values.code
+        this.getToken(authCode)
     }
 
-
+    getToken = authCode =>{
+        axios.post(apihost, {Authorization: `Bearer ${authCode}`})
+          .then(res => {
+            cookies.set("token", res.data.token, {path:"/"})
+            this.renderRedirect();
+          })
+          .catch((error) => {
+            cookies.remove("token", {path:"/"})
+        })
+      }
 
       renderRedirect = () => {
-          return <Redirect to='/v1/login'/>
+          return <Redirect to='/account'/>
       }
 
 
     render() {
         return (
             <>
-                {/* // <div>Auth Code: {this.props.location.search}</div> */}
                 {this.renderRedirect()}
               </>
         )
