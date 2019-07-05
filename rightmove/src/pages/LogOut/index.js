2c061feb8afff6d1bc4d589d63e31f235949c55d
Kasie <kchaplick92@gmail.com>
2019-07-04 20:09:22 -0400

    added logout page

---
+++ rightmove/src/pages/LogOut/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom'
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+class LogOut extends Component {
+
+
+    componentDidMount() {
+        cookies.remove("token", {path:"/"})
+        this.renderRedirect();
+    }
+
+      renderRedirect = () => {
+          return <Redirect to='/'/>
+      }
+
+
+    render() {
+        return (
+            <>
+                {this.renderRedirect()}
+              </>
+        )
+    }
+}
+
+export default LogOut;
