94376aea8cabe19274d2e60ab79cc747baf2629b
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-05 10:56:57 -0400

    styling on login page

--- rightmove/src/pages/ReturnLogin/index.js
+++ rightmove/src/pages/ReturnLogin/index.js
@@ -7,30 +7,22 @@ import "./style.css";
 
 const ReturnLogin = () => {
   return (
-    <div>
+    <div className="center-align">
       <Row>
         <Col size="s12 m12 l12">
           <h4 className="Welcome-back">Welcome back!</h4>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="s12">
+
+ 
           <div>
               <label for="email" className="Label">EMAIL</label>
               <input id="email" type="email" class="validate" placeholder="johndoe@email.com" className="Rectangle"/>
           </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="s12">
+
           <div>
             <label for="password" className="Label">PASSWORD</label>
             <input id="password" type="password" class="validate" placeholder="•••••••••••••••" className="Rectangle" />
           </div>
-        </Col>
-      </Row>
-        <Row>
-          <Col>
+   
             <YellowButton text="Log In" />
           </Col>
         </Row>
