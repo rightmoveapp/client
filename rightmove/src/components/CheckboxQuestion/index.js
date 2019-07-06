73008f662e5cdbb4565d2a24adee46503ae14cce
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-06 10:58:55 -0400

    start styling for the checkbox question input

--- rightmove/src/components/CheckboxQuestion/index.js
+++ rightmove/src/components/CheckboxQuestion/index.js
@@ -3,7 +3,38 @@ import "./style.css";
 
 
 const CheckboxQuestions = () => {
-    return <h1>hello this is a checkbox question</h1>
+    return (
+        <>
+            <form size="col s12 m12 l12">
+                <label className="question">Where in the city would you be happy to work in?</label>
+                <p>
+                    <label>
+                        <input type="checkbox" />
+                        <span className="-Input-Text">Center City</span>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <input type="checkbox" />
+                        <span className="-Input-Text">West</span>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <input type="checkbox" />
+                        <span className="-Input-Text">North</span>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <input type="checkbox" />
+                        <span className="-Input-Text">South</span>
+                    </label>
+                </p>
+            </form>
+        </>
+    )
+
 }
 
 export default CheckboxQuestions;
