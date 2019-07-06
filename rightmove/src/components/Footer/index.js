19cf12dfa136e81eab6b35f3ba424d3761e2edf8
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-06 09:18:40 -0400

    make privacy policy page and link in footer

--- rightmove/src/components/Footer/index.js
+++ rightmove/src/components/Footer/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './style.css'
 
 const Footer = () => {
-    return <footer className="footer Footer-Area Placeholder page-footer">Made with ❤ by Team RedStapler</footer>;
+    return (
+        <footer className="footer Footer-Area Placeholder page-footer">
+            <p>Made with ❤ by Team RedStapler</p>
+            <Link to="/privacy_policy">Privacy Policy</Link>
+        </footer>
+    )
 }
 
 export default Footer;
--- rightmove/src/pages/PrivacyPolicy/index.js
+++ rightmove/src/pages/PrivacyPolicy/index.js
@@ -4,17 +4,18 @@ import Col from '../../components/Col';
 import "./style.css";
 
 const PrivacyPage = () => {
-  return (
-    <>
-      <Row>
-        <Col size="s12 m12 l12">
-          <p>You might feel like some of these questions are pretty personal, so we wanted to let you know ahead of time that we will never sell your data. 
-            Trust & privacy are extremely important to us. So is transparency— for many questions, we have provided an explanation for how we use the information to form your Snapshot. 
-            While most questions are optional, just keep in mind that the more we know, the better your Snapshot will be. </p>
-        </Col>
-      </Row>
-    </>
-  )
+    return (
+        <>
+            <Row>
+                <Col size="s12 m12 l12">
+                    <h4 className="heading">Just a heads up…</h4>
+                    <p>You might feel like some of these questions are pretty personal, so we wanted to let you know ahead of time that <span className="text-style-1">we will never sell your data.</span></p>
+                    <p>Trust & privacy are extremely important to us. So is transparency— for many questions, we have provided an explanation for how we use the information to form your Snapshot.</p>
+                    <p>While most questions are optional, just keep in mind that the more we know, the better your Snapshot will be. </p>
+                </Col>
+            </Row>
+        </>
+    )
 }
 
 export default PrivacyPage;
