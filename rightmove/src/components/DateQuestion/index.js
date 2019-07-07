fb2ad4a79cad2075f5f1d5a8ac51fcfbadb536a0
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-07 18:50:05 -0400

    get date question answer in state

--- rightmove/src/components/DateQuestion/index.js
+++ rightmove/src/components/DateQuestion/index.js
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import YellowButton from "../YellowButton";
 import YellowUnderline from "../YellowUnderline";
 import "./style.css";
 
 
-const DateQuestion = () => {
-    return (
-        <>
-            <form size="col s12 m12 l12">
-                <label className="question">When is your birthday?</label>
-                <input id="birthday" type="date" className="validate" placeholder="10 / 18 / 1992" className="Rectangle" />
-            </form>
-            <Link to="/privacy_policy" target="_blank"><h5 className="explainer">Why do we need this?</h5></Link>
-            <div className="right-align">
-                <YellowUnderline to="/" text="Skip" space="32" />
-                <YellowButton /* to="/" */ text="Continue  →" size="139" /* getNextQuestion={this.getNextQuestion}  *//>
-            </div>
-        </>
-    )
+class DateQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        /* this.handleSubmit = this.handleSubmit.bind(this); */
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    /* handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    } */
+
+    render() {
+        return (
+            <>
+                <form /* onSubmit={this.handleSubmit} */ size="col s12 m12 l12">
+                    <label className="question">When is your birthday?</label>
+                    <input 
+                        value={this.state.value} 
+                        onChange={this.handleChange}
+                        id="birthday" 
+                        type="date" 
+                        className="validate" 
+                        placeholder="10 / 18 / 1992" 
+                        className="Rectangle" 
+                    />
+                    {/* <input type="submit" value="Submit" /> */}
+                </form>
+                <Link to="/privacy_policy" target="_blank"><h5 className="explainer">Why do we need this?</h5></Link>
+                <div className="right-align">
+                    <YellowUnderline to="/" text="Skip" space="32" />
+                    <YellowButton /* to="/" */ text="Continue  →" size="139" /* getNextQuestion={this.getNextQuestion}  */ />
+                </div>
+            </>
+        )
+    }
+
 }
 
 export default DateQuestion;
