567466a63035574d51f6d81238a9a402616281d9
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-07 16:10:42 -0400

    get state updating if you have clicked a particular checkbox

--- rightmove/src/components/CheckboxQuestion/index.js
+++ rightmove/src/components/CheckboxQuestion/index.js
@@ -9,19 +9,40 @@ class CheckboxQuestions extends Component {
     /* state = {
         active: false
     }
+
     onChange = event => {
         // taken straight from the official React Docs
         // https://reactjs.org/docs/forms.html#handling-multiple-inputs
         const target = event.target;
         const value = target.type === "checkbox"
-          ? target.checked
-          : target.value;
+            ? target.checked
+            : target.value;
         const name = target.name;
         this.setState({
-          [name]: value,
-          active:true
+            [name]: value,
+            active: true
         });
-      }; */
+    }; */
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          active: false,
+        };
+    
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+    
+      handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+
 
     render() {
         return (
@@ -30,26 +51,45 @@ class CheckboxQuestions extends Component {
                     <label className="question">Where in the city would you be happy to work in?</label>
                     <p>
                         <label>
-                            <input name="active"
-                                type="checkbox" class="red" />
+                            <input
+                                name="centerCity"
+                                type="checkbox"
+                                checked={this.state.centerCity}
+                                onChange={this.handleInputChange}
+                            />
                             <span className="-Input-Text">Center City</span>
                         </label>
                     </p>
                     <p>
                         <label>
-                            <input type="checkbox" />
+                            <input
+                                name="west"
+                                type="checkbox"
+                                checked={this.state.west}
+                                onChange={this.handleInputChange}
+                            />
                             <span className="-Input-Text">West</span>
                         </label>
                     </p>
                     <p>
                         <label>
-                            <input type="checkbox" />
+                            <input
+                                name="north"
+                                type="checkbox"
+                                checked={this.state.north}
+                                onChange={this.handleInputChange}
+                            />
                             <span className="-Input-Text">North</span>
                         </label>
                     </p>
                     <p>
                         <label>
-                            <input type="checkbox" />
+                            <input
+                                name="south"
+                                type="checkbox"
+                                checked={this.state.south}
+                                onChange={this.handleInputChange}
+                            />
                             <span className="-Input-Text">South</span>
                         </label>
                     </p>
