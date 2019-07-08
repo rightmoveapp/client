8e8dbe4d1319bd641dbff49de9752b299a75be56
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-07 21:36:26 -0400

    ability to render radio buttons dynamically

--- rightmove/src/components/RadioQuestions/index.js
+++ rightmove/src/components/RadioQuestions/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import YellowButton from "../YellowButton";
 import YellowUnderline from "../YellowUnderline";
+import questionChoices from "../../questionsChoices.json"
 import "./style.css";
 
 
@@ -11,18 +12,19 @@ class RadioQuestions extends Component {
         super();
 
         this.state = {
+            questionChoices,
             choice: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
-       /*  this.handleSubmit = this.handleSubmit.bind(this); */
+        /*  this.handleSubmit = this.handleSubmit.bind(this); */
     }
 
     handleChange(event) {
         this.setState({
             choice: event.target.value
         });
-    }
+    };
 
     /* handleFormSubmit = (formSubmitEvent) => {
         formSubmitEvent.preventDefault();
@@ -30,12 +32,57 @@ class RadioQuestions extends Component {
         console.log('You have selected:', this.state.selectedOption);
     } */
 
-    render() {
+    figureOutJson(props) {
+        /* console.log(this.state.questionChoices); */
+        console.log(this.props.questionText);
+        console.log(this.props.questionChoices[0][0].choice_text);
+    }
+
+
+    render(props) {
         return (
             <>
-                <form onSubmit={this.handleSubmit} size="col s12 m12 l12">
+                <form /* onSubmit={this.handleSubmit} */ size="col s12 m12 l12">
+                    {this.figureOutJson()}
+                    <label className="question">{this.props.questionText}</label>
+                    <input id={this.props.questionId} type={this.props.questionType} className="validate" className="Rectangle" />
+                    {this.props.questionChoices[0].map(questionChoice => (
+                        <p>
+                            <label>
+                                <input
+                                    key={questionChoice.id}
+                                    value={questionChoice.choice_text}
+                                    type={questionChoice.input_type}
+                                    checked={this.state.choice === questionChoice.choice_text}
+                                    onChange={this.handleChange}
+                                />
+                                <span className="-Input-Text">{questionChoice.choice_text}</span>
+                            </label>
+                        </p>
+
+                    ))}
+
+
+{/* 
+--------------------------------
+                     <label className="question">{this.state.questionChoices.questionsAndChoices[1].question.question_text}</label>
+                    <input id={this.state.questionChoices.questionsAndChoices[1].question.id} type={this.state.questionChoices.questionsAndChoices[1].question.input_type} className="validate" className="Rectangle" /> 
+                    {this.state.questionChoices.map(questionChoice => (
+                        <p>
+                            <label>
+                                <input
+                                    value={questionChoice.choices.choice_text}
+                                    type={questionChoice.question.input_type}
+                                    checked={this.state.choice === questionChoice.choices.choice_text}
+                                    onChange={this.handleChange}
+                                />
+                                <span className="-Input-Text">{questionChoice.choices.choice_text}</span>
+                            </label>
+                        </p>
+
+                    ))}
                     <label className="question">What are you pronouns?</label>
-                    <input id="pronouns" type="radio" className="validate" className="Rectangle" />
+                    <input id={questionChoice.id} type={questionChoice.input_type} className="validate" className="Rectangle" />
                     <p>
                         <label>
                             <input
@@ -79,7 +126,7 @@ class RadioQuestions extends Component {
                             />
                             <span className="-Input-Text">Brown</span>
                         </label>
-                    </p>
+                    </p> */}
                 </form>
                 <Link to="/privacy_policy" target="_blank"><h5 className="explainer">Why do we need this?</h5></Link>
                 <div className="right-align">
