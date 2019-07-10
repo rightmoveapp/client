0e3ff0f3f8aba61557af77c75d5fdf4ac121dec3
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-10 10:53:18 -0400

    get styling on current job correct

---
+++ rightmove/src/components/JobList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css'
+
+const JobList = () => {
+    return (
+        <h4>Job1</h4>
+    )
+}
+
+export default JobList;
--- rightmove/src/pages/Account/index.js
+++ rightmove/src/pages/Account/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Col from '../../components/Col';
 import Row from '../../components/Row';
+import JobList from '../../components/JobList';
 import './style.css';
 import YellowUnderline from "../../components/YellowUnderline";
 
@@ -14,16 +15,23 @@ class Account extends Component {
             <div className="There-are-new-questi YellowRectangle">
               <p className="Add-Overall-Padding">
                 There are new questions for you to answer!
-                
-                <span className="Add-Space-Left Remove-Bold"><YellowUnderline to="/questions" text="Answer Questions"/></span>
+
+                <span className="Add-Space-Left Remove-Bold"><YellowUnderline to="/questions" text="Answer Questions" /></span>
               </p>
             </div>
             <div className="WhiteRectangle">
-              <h4 className="Current-Job-Title">UX Designer</h4>
-              <h2 className="Current-Job-Company">RevZilla</h2>
+              <div>
+                <h4 className="Current-Job-Title">UX Designer</h4>
+                <h2 className="Current-Job-Company">RevZilla</h2>
+                <YellowUnderline to="/job" text="Edit" />
+              </div>
               <div className="Oval">
                 <h4 className="XX">XX%</h4>
               </div>
+            </div>
+            <div className="WhiteRectangle-Jobs">
+              <h4>Top Gigs</h4>
+              <JobList />
             </div>
           </Col>
         </Row>
