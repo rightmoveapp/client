d04f09a1254e89f7c1f6686ff02b70d347f0003d
Kasie <kchaplick92@gmail.com>
2019-07-11 16:48:27 -0400

    date onclick and submit

--- rightmove/src/components/DateQuestion/index.js
+++ rightmove/src/components/DateQuestion/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import API from "../../utils/API"
 import YellowButton from "../YellowButton";
 import YellowUnderline from "../YellowUnderline";
 import "./style.css";
@@ -8,42 +9,60 @@ import "./style.css";
 class DateQuestion extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = {
+            question:[],
+            value: ''
+        };
 
         this.handleChange = this.handleChange.bind(this);
         /* this.handleSubmit = this.handleSubmit.bind(this); */
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({
+            value: event.target.value,
+            question: this.props.questionId
+        });
     }
 
-    /* handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    } */
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log("clicked")
+        API.postUserAttrAnswers({
+            question: this.state.question,
+            answer: this.state.value,
+        })
+            .then(response => {
+                this.props.setAnsweredQuestion(this.state.question)
+                this.props.getRandomQuestion()
+            }
+            )
+            .catch(err => console.log(err));
+
+        console.log('You have selected:', this.state.selectedOption);
+    }
 
     render(props) {
         return (
             <>
                 <h4 className="heading">Here we go! Tell us a little about yourself.</h4>
-                <form /* onSubmit={this.handleSubmit} */ size="col s12 m12 l12">
+                <form onSubmit={this.handleFormSubmit} size="col s12 m12 l12">
                     <label className="question">{this.props.questionText}</label>
-                    <input 
-                        value={this.state.value} 
+                    <input
+                        value={this.state.value}
                         onChange={this.handleChange}
-                        id={this.props.questionId} 
-                        type={this.props.questionType} 
+                        id={this.props.questionId}
+                        type={this.props.questionType}
                         className="validate"
-                        placeholder={this.props.questionPlaceholder}  
-                        className="Rectangle" 
+                        placeholder={this.props.questionPlaceholder}
+                        className="Rectangle"
                     />
                     {/* <input type="submit" value="Submit" /> */}
                 </form>
                 <Link to="/privacy_policy" target="_blank"><h5 className="explainer">Why do we need this?</h5></Link>
                 <div className="right-align">
                     <YellowUnderline to="/" text="Skip" space="32" />
-                    <YellowButton /* to="/" */ text="Continue  →" size="139" /* getNextQuestion={this.getNextQuestion}  */ />
+                    <YellowButton type="submit" onClick={this.handleFormSubmit} text="Continue  →" size="139" /* getNextQuestion={this.getNextQuestion}  */ />
                 </div>
             </>
         )
