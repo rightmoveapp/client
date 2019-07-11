c66b8e8a97f6f90701c93279e4ddab3fb381937f
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-11 15:14:04 -0400

    add three hard coded questions to job page and then left spacing on text input

--- rightmove/src/pages/JobQuestions/index.js
+++ rightmove/src/pages/JobQuestions/index.js
@@ -27,7 +27,54 @@ class JobQuestions extends Component {
       <>
         <Row>
           <Col size="s12 m12 l12">
-            <p>I am a job</p>
+            <h4 className="heading">Give us the deets on this gig 🤑</h4>
+            <form /* onSubmit={this.handleSubmit} */ size="col s12 m12 l12">
+            <label className="question">Company name:</label>
+              <input
+                /* value={this.state.value} */
+                onChange={this.handleChange}
+                /* name="firstName" */
+                id="22"
+                type="text"
+                className="validate"
+                placeholder="Best place in the world"
+                className="Rectangle"
+              />
+              <label className="question">Position title:</label>
+              <input
+                /* value={this.state.value} */
+                onChange={this.handleChange}
+                /* name="firstName" */
+                id="23"
+                type="text"
+                className="validate"
+                placeholder="Web Developer"
+                className="Rectangle"
+              />
+              <label className="question">Annual salary:</label>
+              <input
+                /* value={this.state.value} */
+                onChange={this.handleChange}
+                /* name="firstName" */
+                id="24"
+                type="text"
+                className="validate"
+                placeholder="$80,000"
+                className="Rectangle"
+              />
+              <label className="question">{this.props.questionText}</label>
+              <input
+                /* value={this.state.value} */
+                onChange={this.handleChange}
+                /* name="firstName" */
+                id={this.props.questionId}
+                type={this.props.questionType}
+                className="validate"
+                placeholder={this.props.questionPlaceholder}
+                className="Rectangle"
+              />
+              {/* <input type="submit" value="Submit" /> */}
+            </form>
           </Col>
         </Row>
 
