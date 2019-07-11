50108aabafcb5c59af3a3cb5809af019f06c403a
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-11 16:22:17 -0400

    get at one question rednering from the databased underneith the hard coded questions at the top of the jobs page

--- rightmove/src/pages/JobQuestions/index.js
+++ rightmove/src/pages/JobQuestions/index.js
@@ -2,9 +2,25 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import Row from '../../components/Row';
 import Col from '../../components/Col';
+import CheckboxQuestion from '../../components/CheckboxQuestion';
+import DateQuestion from '../../components/DateQuestion';
+import RadioQuestions from '../../components/RadioQuestions';
+import TextQuestion from '../../components/TextQuestion';
 import "./style.css";
 
 class JobQuestions extends Component {
+    constructor(props) {
+      super(props)
+
+      /* this.handleInputChange = this.handleInputChange.bind(this) */
+
+  }
+
+  state = {
+      jobQuestions: [],
+      questionType: []
+  };
+
   componentDidMount() {
     this.loadQuestions();
   }
@@ -12,14 +28,71 @@ class JobQuestions extends Component {
   loadQuestions = () => {
     API.getJobQuestions()
       .then(response => {
-        this.setState({ questionsChoices: response.data.questionsAndChoices })
-        console.log(response.data.questionsAndChoices)
-        /* this.getRandomQuestion() */
+        this.setState({ jobQuestions: response.data.questionsAndChoices })
+        console.log(this.state.jobQuestions);
+        console.log(this.state.jobQuestions[0].input_type);
+        this.getQuestions()
       }
       )
-      // TODO: Handle if user has answered all questions
       .catch(err => console.log(err));
   };
+
+  getQuestions = () => {
+    for(let i = 0; i < this.state.jobQuestions.length; i++) {
+      console.log(this.state.jobQuestions[i].input_type)
+      // TODO: figure out how to fix this
+      if (this.state.jobQuestions[i].input_type === "radio") {
+        this.setState({
+            questionType:
+            <RadioQuestions
+              key={this.state.jobQuestions[i].id}
+              questionId={this.state.jobQuestions[i].id}
+              questionText={this.state.jobQuestions[i].question_text}
+              questionType={this.state.jobQuestions[i].input_type}
+              questionChoices={[this.state.jobQuestions[i].choices]}
+            />
+        })
+
+      }
+      else if (this.state.jobQuestions[i].input_type === "date") {
+        this.setState({
+            questionType:
+            <DateQuestion
+              questionId={this.state.jobQuestions[i].id}
+              questionText={this.state.jobQuestions[i].question_text}
+              questionType={this.state.jobQuestions[i].input_type}
+              questionPlaceholder={this.state.jobQuestions[i].placeholder}
+            />
+        })
+      }
+      else if (this.state.jobQuestions[i].input_type === "checkbox") {
+        this.setState({
+            questionType:
+            <CheckboxQuestion
+              key={this.state.jobQuestions[i].id}
+              questionId={this.state.jobQuestions[i].id}
+              questionText={this.state.jobQuestions[i].question_text}
+              questionType={this.state.jobQuestions[i].input_type}
+              questionChoices={[this.state.jobQuestions[i].choices]}
+            />
+        })
+      }
+      else {
+        this.setState({
+            questionType:
+            <TextQuestion
+              questionId={this.state.jobQuestions[i].id}
+              questionText={this.state.jobQuestions[i].question_text}
+              questionType={this.state.jobQuestions[i].input_type}
+              questionPlaceholder={this.state.jobQuestions[i].placeholder}
+            />
+        })
+      }
+      /* this.setState({currentQuestion:randomQuestion}) */
+
+    }
+
+  }
 
 
   render() {
@@ -27,9 +100,10 @@ class JobQuestions extends Component {
       <>
         <Row>
           <Col size="s12 m12 l12">
+            <>
             <h4 className="heading">Give us the deets on this gig 🤑</h4>
             <form /* onSubmit={this.handleSubmit} */ size="col s12 m12 l12">
-            <label className="question">Company name:</label>
+            <label className="question">Company name</label>
               <input
                 /* value={this.state.value} */
                 onChange={this.handleChange}
@@ -40,7 +114,7 @@ class JobQuestions extends Component {
                 placeholder="Best place in the world"
                 className="Rectangle"
               />
-              <label className="question">Position title:</label>
+              <label className="question">Position title</label>
               <input
                 /* value={this.state.value} */
                 onChange={this.handleChange}
@@ -51,7 +125,7 @@ class JobQuestions extends Component {
                 placeholder="Web Developer"
                 className="Rectangle"
               />
-              <label className="question">Annual salary:</label>
+              <label className="question">Annual salary</label>
               <input
                 /* value={this.state.value} */
                 onChange={this.handleChange}
@@ -62,19 +136,10 @@ class JobQuestions extends Component {
                 placeholder="$80,000"
                 className="Rectangle"
               />
-              <label className="question">{this.props.questionText}</label>
-              <input
-                /* value={this.state.value} */
-                onChange={this.handleChange}
-                /* name="firstName" */
-                id={this.props.questionId}
-                type={this.props.questionType}
-                className="validate"
-                placeholder={this.props.questionPlaceholder}
-                className="Rectangle"
-              />
               {/* <input type="submit" value="Submit" /> */}
+              {this.state.questionType}
             </form>
+            </>
           </Col>
         </Row>
 
