3b94387e507ce9adacf0ef89b1fa0c1edd4348d6
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-13 10:57:04 -0400

    get checkbox question answers into state

--- rightmove/src/pages/BasicQuestions/index.js
+++ rightmove/src/pages/BasicQuestions/index.js
@@ -43,13 +43,13 @@ class BasicQuestions extends Component {
     });
   };
 
-  handleCheckBoxChange = (event) => {
-    const target = event.target.id;
+  handleCheckBoxChange= (event) =>{
+    const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
     this.setState({
-      [name]: value,
+        [name]: value,
       choice: [...this.state.choice, name],
       /* question: this.state.currentQuestion.id */
     });
