d6016695dc6310186ba4d56c12cb70b1c0a79f7e
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-14 19:48:52 -0400

    fixed issue with inter bold font not showing up

--- rightmove/src/components/Logo/index.js
+++ rightmove/src/components/Logo/index.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from './logo.svg';
 import "./style.css"
 
 const LogoContainer = () => {
-    return (
-      <Link to="/account" className="no-underline">
-          <Logo />
-      </Link>
-    )
-  }
-  
-  export default LogoContainer;
+  return <Logo />
+}
+
+export default LogoContainer;
