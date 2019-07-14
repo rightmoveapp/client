3168476801a451db2d76b4c3a5f4d08cf1f098f6
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-14 18:24:12 -0400

    add logic for if the question counter reaches 0 to add the link to add a job

--- rightmove/src/components/Countdown/index.js
+++ rightmove/src/components/Countdown/index.js
@@ -3,16 +3,31 @@ import YellowUnderline from "../YellowUnderline";
 import "./style.css";
 
 const Countdown = (props) => {
-    return (
-        <>
-            <div className="There-are-new-questi Remove-Bold">
-                <p className="Add-Overall-Padding">
-                    Answer <span className="Add-Bold">{props.count}</span> before you add a job.
+    if (props.count === 0) {
+        return (
+            <>
+                <div className="There-are-new-questi Remove-Bold">
+                    <p className="Add-Overall-Padding">
+                        Answer <span className="Add-Bold">{props.count}</span> before you add a job.
                 <span className="Add-Space-Left"><YellowUnderline to="/questions" text="Add a job" /></span>
-                </p>
-            </div>
-        </>
-    );
+                    </p>
+                </div>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <div className="There-are-new-questi Remove-Bold">
+                    <p className="Add-Overall-Padding">
+                        Answer <span className="Add-Bold">{props.count}</span> before you add a job.
+                    {/* <span className="Add-Space-Left"><YellowUnderline to="/questions" text="Add a job" /></span> */}
+                    </p>
+                </div>
+            </>
+        );
+    }
+    
 }
 
 export default Countdown;
