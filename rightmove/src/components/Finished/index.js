d487f1718baeb3d1e8f7b67f5cc173d17d44e348
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-07-15 17:41:05 -0400

    add styling to the finished page

--- rightmove/src/components/Finished/index.js
+++ rightmove/src/components/Finished/index.js
@@ -8,14 +8,11 @@ const Finished = () => {
     return (
         <>
         <Row>
-          <Col size="s12 m12 l12" className="align-left">
-            <div className="Ooh-yeah-about-tha">You've finished all the questions at this moment</div>
+          <Col size="s12 m12 l12" className="align-vertical">
+            <div className="Ooh-yeah-about-tha">You've finished all the questions at this moment <span role="img" aria-label="celebration emoji">🥳</span></div>
             <p className="Looks-like-someone-s">Looks like you should add jobs now.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <RedButton text='Go To Job Questions →' to="/jobquestions"/>
+          
+            <RedButton text='Job Questions →' to="/jobquestions"/>
           </Col>
         </Row>
       </>
